87d75a41c98c5b6418d33bc9f8d54c183dcd87f3
Artur <buch16@bk.ru>
2023-01-30 22:31:28 +0300

    Выполнил дз

---
+++ dz1.py
@@ -0,0 +1,41 @@
+# Задание 1
+# number = int(input("Введите число: "))
+# number_1 = number // 100
+# number_2 = (number % 100) // 10
+# number_3 = number % 10
+# sum = number_1 + number_2 + number_3
+# print(sum)
+
+
+# Задание 2
+name_1 = int(input("Петя сделал: "))
+name_2 = int(input("Сережа сделал: "))
+name_3 = (name_1 + name_2) * 2
+print(f"Катя сделала: {name_3}, потому что Петя сделал {name_1}, и Сережа сделал {name_2}")
+
+
+# Задание 3
+# number = int(input("Введите номер билетика: "))
+# number_1 = number // 100000
+# number_2 = (number // 10000) % 10
+# number_3 = (number // 1000) % 10
+# sum_1 = number_1 + number_2 + number_3
+# number_4 = (number // 100) % 10
+# number_5 = (number // 10) % 10
+# number_6 = number  % 10
+# sum_2 = number_4 + number_5 + number_6
+# if  number > 99999:
+#     if number <= 999999:
+#         sum_1 == sum_2
+#         print("Счастливый")
+# else:
+#     print("Несчастливый или не подходит под условие задачи")
+
+# Задание 4
+#n = 3
+#m = 1
+#k = 2
+#if k % n == 0 or k % m == 0:
+#    print("Yes")
+#else:
+#    print("No")
